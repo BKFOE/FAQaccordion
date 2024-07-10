72c8dfcfe85faef75e7683a1f35376c9dd788de6
bkfoe <tirrell.barronette@gmail.com>
2024-07-09 23:46:20 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 $(document).ready(function() {
 
-    function toggleAccoridon(button) {
-
+    function toggleAccordion(button) {
         // Toggle the collapsed class 
         $(button).toggleClass("collapsed"); 
 
         // Show or hide the paragraph based on the presence of the collapsed class
-        if ($(button).hasClass("collpased")) {
+        if ($(button).hasClass("collapsed")) {
             $(button).closest(".accordion-item").find(".accordion-body").show();
         } else {
             $(button).closest(".accordion-item").find(".accordion-body").hide();
         }
     }
 
-    $(button).click(function() {
-
-        // Toggle the collapsed class 
-        toggleAccoridon(this);
+    $(".accordion-button").click(function() {
+        toggleAccordion(this);
     });
 
     $(".accordion-button").keydown(function(event) {
-        // Check if enter or space is pressed 
-        if(event.key === "Enter" || event.key === " ") {
-            toggleAccoridon(this);
+        // Check if Enter or Space is pressed 
+        if (event.key === "Enter" || event.key === " ") {
+            toggleAccordion(this);
             event.preventDefault();
-        };
+        }
     });
 });
-
-       
